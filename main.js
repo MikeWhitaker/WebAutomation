13a44ba109649314b41b93fe2b11584713b027ac
DESKTOP-3SE1LP0\Mike <whitaker.mj@gmail.com>
2019-02-23 09:22:52 +0100

    makes screenshot of two sites

--- main.js
+++ main.js
@@ -1,5 +1,9 @@
 var casper = require('casper').create();
 casper.start('http://www.google.nl/', function(){
-	this.capture('./output/test.png')
+	this.capture('./output/test.png');
+});
+casper.thenOpen('http://www.bing.com/', function(){
+	console.log('inside second statement');
+	this.capture('./output/test02.png');
 });
 casper.run();
